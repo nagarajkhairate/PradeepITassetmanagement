2752de516a3f7c9e586ca18c3aabbe24167dbcbf
Sanjeevpradeepit <sanjeev.k@in.pradeepit.com>
2024-06-06 10:15:58 +0530

    alignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-import { Box } from "@mui/joy";
+import { Box } from '@mui/joy'
 
-import MainLayout from "./components/layout/MainLayout";
-import { routes } from "./routes";
+import MainLayout from './components/layout/MainLayout'
+import { routes } from './routes'
 
 const App = () => {
   return (
     <Box>
-      
-        <Routes>
-          {/* <Route path="/login" element={<Login />} />
+      <Routes>
+        {/* <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} /> */}
         <Route path="/" element={<MainLayout />}>
           {routes}
         </Route>
-        </Routes>
-      </Box>
+      </Routes>
+    </Box>
+  )
+}
 
-  );
-};
-
-export default App;
+export default App
