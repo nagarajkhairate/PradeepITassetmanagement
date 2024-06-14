7f9c6f36f9173725678513f339d8e9aa3820f982
SoumyaTechie <soumya.s@in.pradeepit.com>
2024-06-14 15:38:55 +0530

    companySitesJson

--- src/components/Common/AppFile/FileField.tsx
+++ src/components/Common/AppFile/FileField.tsx
@@ -3,6 +3,8 @@ import FormControl from '@mui/joy/FormControl';
 import FormLabel from '@mui/joy/FormLabel';
 import FormHelperText from '@mui/joy/FormHelperText';
 import Input from '@mui/joy/Input';
+import { IconButton, Typography } from '@mui/joy';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 type FieldProps = {
   id: number;
@@ -25,6 +27,29 @@ interface InputFieldProps {
 }
 
 const FileField: React.FunctionComponent<InputFieldProps> = ({ field, formData, handleInputChange }) => {
+  const [imagePreview, setImagePreview] = React.useState<string | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const file = e.target.files?.[0];
+  
+  if (file) {  
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const result = reader.result as string;
+      setImagePreview(result);
+    };
+    reader.readAsDataURL(file);
+
+    if (handleInputChange) {
+      handleInputChange(e);
+    }
+  }
+};
+
+const handleDelete = () => {
+  setImagePreview(null);
+};
+
   return (
     <FormControl>
       <FormLabel>
@@ -35,9 +60,22 @@ const FileField: React.FunctionComponent<InputFieldProps> = ({ field, formData, 
         value={formData[field.value] || ''} 
         name={field.value} 
         type={field.dataType} 
-        onChange={handleInputChange}
+        onChange={handleFileChange}
         required={field.required}
-      />
+        style={{ display: imagePreview ? 'none' : 'block' }}
+      />  
+       {imagePreview && (
+        <div style={{ marginTop: '10px', position: 'relative' }}>
+          <img src={imagePreview} alt="Preview" style={{ height: "100px", width: "auto" }} />  
+          <IconButton
+            onClick={handleDelete}
+            sx={{ position: 'relative', color: 'black' }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </div> 
+      )} 
+       <Typography level="body-xs">Only (JPG, GIF, PNG) Allowed</Typography> 
       <FormHelperText>This is a helper text.</FormHelperText>
     </FormControl>
   );
